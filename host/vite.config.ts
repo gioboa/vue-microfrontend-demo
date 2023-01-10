7b4e2f86b8c864f9afcdf24c43bf1716093a2dcb
gioboa <giorgiob.boa@gmail.com>
2023-01-10 09:08:31 +0100

    add host

---
+++ host/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { federation } from '@module-federation/vite';
+import { createEsBuildAdapter } from '@softarc/native-federation-esbuild';
+
+export default defineConfig(async ({ command }) => ({
+	server: {
+		fs: {
+			allow: ['.', '../shared'],
+		},
+	},
+	plugins: [,
+		await federation({
+			options: {
+				workspaceRoot: __dirname,
+				outputPath: 'dist',
+				tsConfig: 'tsconfig.json',
+				federationConfig: 'module-federation/federation.config.cjs',
+				verbose: false,
+				dev: command === 'serve',
+			},
+			adapter: createEsBuildAdapter({plugins: [],}),
+		}),
+		vue(), vueJsx(),
+	]
+}));
