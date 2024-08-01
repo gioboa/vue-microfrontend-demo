d3a7d624a3b263c0c1502009cb5c4054823b9702
gioboa <giorgiob.boa@gmail.com>
2024-08-01 14:49:54 +0200

    chore: remove bootstrap files

--- host/src/main.ts
+++ host/src/main.ts
@@ -1,3 +1,12 @@
-import("./bootstrap");
-export { };
+import { createPinia } from "pinia";
+import { state } from "shared";
+import { createApp } from "vue";
+import "./stores/counter";
 
+state.message = "Hello from host!";
+
+import("./App.vue").then((App) => {
+  const app = createApp(App.default);
+  app.use(createPinia());
+  app.mount("#app");
+});
--- remote/src/main.ts
+++ remote/src/main.ts
@@ -1,10 +1,8 @@
-// import { initFederation } from "@softarc/native-federation";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import App from "./App.vue";
 
-(async () => {
-  // await initFederation();
+const app = createApp(App);
 
-  await import("./bootstrap");
-})();
-
-export { };
-
+app.use(createPinia());
+app.mount("#app");
